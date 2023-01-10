36a190d4de316c81d0361052f1157ec9a4d6bf1d
Lauren Pace <lauren.pace-2@selu.edu>
2023-01-09 20:59:49 -0600

    methods

---
+++ Methods/MultiplyMethod.java
@@ -0,0 +1,21 @@
+package class_examples.Methods;
+
+public class MultiplyMethod {
+    public static void main(String[] args) {
+        //calling method with literals
+        double firstOutput = multiplyTwoNums(4, 5.0);
+
+        int myNum1 = 4;
+        double myNum2 = 5.0;
+
+        //calling method with variables
+        double secondOutput = multiplyTwoNums(myNum1, myNum2);
+
+        System.out.println(firstOutput);
+        System.out.println(secondOutput);
+    }
+
+    public static double multiplyTwoNums(double num1, double num2){
+        return num1 * num2;
+    }
+}
